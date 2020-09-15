5f76a3101add2de6e23e5f0eae2b5f396c6ed494
Diego Guerrero <diegokmc-2013@hotmail.com>
2020-09-14 20:34:43 -0500

    Give initial movement to player

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0)*Time.deltaTime*speed);
+
+
+    }
+}
