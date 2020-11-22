a49b0a1f655331195d494cc6e7290b126a9c6e87
Donaldo Salazar <donaldosalazar1@gmail.com>
2020-11-22 04:47:09 -0600

    Pause Assets

---
+++ Assets/Scenes/Menu Assets/Pause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
